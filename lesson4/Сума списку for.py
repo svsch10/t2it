ffc44d0b133ab014b1df2d6209830f995e15e7ad
oleksandr_s <sv.sch10@gmail.com>
2023-04-15 22:56:58 +0300

    lesson4

--- lesson4/Сума списку for.py
+++
@@ -1,5 +0,0 @@
-a = [int(x) for x in input('Введіть 5 цілих чисел: ').split()]
-s = 0
-for item in a:
-    s += item
-print(s)
--- lesson4/Сума списку while.py
+++
@@ -1,7 +0,0 @@
-a = [int(x) for x in input('Введіть 5 цілих чисел: ').split()]
-s = 0
-i = 0
-while i<5:
-    s += a[i]
-    i += 1
-print(s)
